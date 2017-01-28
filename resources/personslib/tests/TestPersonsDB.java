0da9751f0496dbe198a7924e7ddf0bec3fd547ca
florianQgb <quaghebeur.f@gmail.com>
2017-01-28 16:35:56 +0100

    WIP

--- resources/personslib/tests/TestPersonsDB.java
+++ resources/personslib/tests/TestPersonsDB.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 public class TestPersonsDB {
 
 	protected static PersonDB l = new PersonDB();
-	protected static IPersonDB insance;
+	protected static IPersonDB instance;
 
 	//test hash
 	@Test

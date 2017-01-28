03c1d09271759da104310ecbcdd8cba724e6c625
florianQgb <quaghebeur.f@gmail.com>
2017-01-28 16:45:10 +0100

    WIP

--- resources/personslib/tests/TestPersonsDB.java
+++ resources/personslib/tests/TestPersonsDB.java
@@ -14,7 +14,7 @@ import persons.IPersonDB;
 
 import org.junit.Test;
 
-public class TestPersonsDB extends TestCase {
+public class TestPersonsDB {
 
 	protected static PersonDB l = new PersonDB();
 	protected static IPersonDB instance;

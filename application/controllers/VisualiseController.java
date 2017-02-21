b9437fd10b446e4bdd0acd84e20821967331c31a
plabadille <plabadille@gmail.com>
2017-02-21 17:19:36 +0100

    add fragment and link a css file

--- application/controllers/VisualiseController.java
+++ application/controllers/VisualiseController.java
@@ -4,11 +4,6 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.ui.Model;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.List;
 
 @Controller
 public class VisualiseController {
--- application/web/pages/home.jsp
+++ application/web/pages/home.jsp
@@ -1,3 +1,9 @@
+<jsp:include page="fragments/header.jsp" />
+
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 
 <h1>Homestay Network</h1>
+
+<p id="test">test</p>
+
+<jsp:include page="fragments/footer.jsp" />
